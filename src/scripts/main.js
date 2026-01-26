71641a291ed275b8d3d1930cb45a1a8d4dde0d75
DanMan3 <dhbennion@gmail.com>
2026-01-26 13:28:58 -0700

    project files moved into src folder for Render deployment

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@ import navInit from "./navigation.mjs";
 import footerInit from "./dates.mjs";
 
 document.addEventListener("DOMContentLoaded", () => {
-    navInit();
-    footerInit();
+  navInit();
+  footerInit();
+  console.log('Env:', import.meta.env);
 });
